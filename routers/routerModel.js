1c0833ceed310bad6541fa9d662d77dd20eeb418
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-08 09:06:05 +0000

    create function to get all projects

--- routers/routerModel.js
+++ routers/routerModel.js
@@ -3,3 +3,12 @@ const db = require('../data/dbConfig');
 function getResource () {
     return db('resource')
 }
+
+function getProject () {
+    return db('project')
+}
+
+module.exports = {
+    getResource,
+    getProject,
+}
