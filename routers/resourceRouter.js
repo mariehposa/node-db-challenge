cfa9e582835b110ad2b8843aa95d19ec194365ba
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-08 09:30:23 +0000

    import project, resource and task router to server

--- routers/resourceRouter.js
+++ routers/resourceRouter.js
@@ -1,3 +1,4 @@
 const express = require('express');
 const db = require('./routerModel');
 const router = express.Router();
+
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
+const projectRouter = require('./routers/projectRouter')
+const resourceRouter = require('./routers/resourceRouter')
+const taskRouter = require('./routers/taskRouter')
 
 const server = express()
 server.use(helmet());
 server.use(express.json());
+
+
 
 server.get('/', (req, res) => {
     res.send('Its working!')
