9ef0927191cf45232a42abf2241991578cd07818
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-08 09:48:55 +0000

    create endpoint to fetch all projects

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -2,4 +2,16 @@ const express = require('express');
 const db = require('./routerModel');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    db.getProject()
+    .then (project => {
+        res.status(200).json(project)
+    })
+    .catch(err => {
+        res.status(404).json({
+            message: "Couldn't fetch" + err.message
+        })
+    })
+})
+
 module.exports = router;
