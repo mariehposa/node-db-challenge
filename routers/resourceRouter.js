f27e809fc19fca9915d5a7635dfddf24158eb760
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-08 09:41:31 +0000

    create endpoint to fetch all resources

--- routers/resourceRouter.js
+++ routers/resourceRouter.js
@@ -2,3 +2,16 @@ const express = require('express');
 const db = require('./routerModel');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    db.getResource()
+    .then(resource => {
+        res.status(200).json(resource)
+    })
+    .catch(err => {
+        res.status(404).json({
+            message: "Couldn't fetch" + err.message
+        })
+    })
+})
+
+module.exports = router;
