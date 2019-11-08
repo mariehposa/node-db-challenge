4065d141bd57aaa3dccd9947556e0823b95f13ee
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-08 09:47:12 +0000

    create endpoint to fetch all tasks

--- routers/taskRouter.js
+++ routers/taskRouter.js
@@ -2,4 +2,16 @@ const express = require('express');
 const db = require('./routerModel');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    db.getTask()
+    .then (task => {
+        res.status(200).json(task)
+    })
+    .catch(err => {
+        res.status(404).json({
+            message: "Couldn't fetch" + err.message
+        })
+    })
+})
+
 module.exports = router;
