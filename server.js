d074979c927e76715f11406a254300adc0b47152
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-08 09:32:35 +0000

    create api for each router

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const server = express()
 server.use(helmet());
 server.use(express.json());
 
-
+server.use('/api/project', projectRouter);
+server.use('/api/resource', resourceRouter);
+server.use('/api/task', taskRouter);
 
 server.get('/', (req, res) => {
     res.send('Its working!')
