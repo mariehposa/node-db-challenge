40af80f1ac45fb9a0048c4d99cd55a5eb3abb1e1
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-08 08:27:19 +0000

    create table for task

--- data/migrations/20191108081853_create-tables.js
+++ data/migrations/20191108081853_create-tables.js
@@ -20,6 +20,20 @@ exports.up = function(knex) {
             .unique()
         table.string('description')
     })
+    .createTable('task', table => {
+        table.increments('id')
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('project')
+        table.string('description')
+            .notNullable()
+        table.string('notes')
+        table.boolean('completed')
+            .defaultTo(0)
+            .notNullable()
+    })
 };
 
 exports.down = function(knex) {
