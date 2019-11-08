ee8936bfa5415966d350ce30eab800b5d348ef80
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-08 08:16:33 +0000

    configure knexfile settings

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,21 @@
 // Update with your config settings.
-
 module.exports = {
-
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      filename: './data/project.db3'
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     },
     pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+      afterCreate: (connections, done) => {
+        connections.run('PRAGMA foreign_keys = ON', done);
+      }
     }
   }
-
 };
